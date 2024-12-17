a6b12c037efe4ac4cc19119267701fcde8b810e1
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2024-12-17 15:52:39 +0100

    initial 🍇

--- src/main/kotlin/no/nav/eux/adresse/oppdatering/integration/config/IntegrationConfig.kt
+++ src/main/kotlin/no/nav/eux/adresse/oppdatering/integration/config/IntegrationConfig.kt
@@ -3,10 +3,12 @@ package no.nav.eux.adresse.oppdatering.integration.config
 import no.nav.eux.adresse.oppdatering.integration.security.BearerToken
 import no.nav.eux.adresse.oppdatering.integration.security.BearerTokenService
 import no.nav.eux.adresse.oppdatering.integration.security.BearerTokenService.Client
-import no.nav.eux.adresse.oppdatering.integration.security.ClientProperties.ApiClient
+import no.nav.eux.adresse.oppdatering.integration.security.BearerTokenService.Client.EUX_RINA_API
+import no.nav.eux.adresse.oppdatering.integration.security.ClientProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.web.client.RestClient
 
 @Configuration
 class IntegrationConfig {
@@ -14,15 +16,22 @@ class IntegrationConfig {
     @Bean
     fun clientTokens() = HashMap<Client, BearerToken>()
 
-    fun bearerTokenInterceptor(
-        bearerTokenService: BearerTokenService,
+    @Bean
+    fun euxRinaApi(
+        clientProperties: ClientProperties,
+        bearerTokenService: BearerTokenService
+    ) = RestClient
+        .builder()
+        .baseUrl("${clientProperties.euxRinaApi.url}/cpi")
+        .requestInterceptor(bearerTokenService interceptorFor EUX_RINA_API)
+        .build()
+
+    infix fun BearerTokenService.interceptorFor(
         client: Client
-    ): ClientHttpRequestInterceptor {
-        return ClientHttpRequestInterceptor { request, body, execution ->
-            val token = bearerTokenService.fetch(client).token
-            request.headers.setBearerAuth(token)
-            execution.execute(request, body)
-        }
+    ) = ClientHttpRequestInterceptor { request, body, execution ->
+        val token = fetch(client).token
+        request.headers.setBearerAuth(token)
+        execution.execute(request, body)
     }
 
 }
