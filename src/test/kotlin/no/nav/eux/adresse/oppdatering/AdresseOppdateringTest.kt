04b377db8c6625c2dd25f318e3c40ffde06bf775
Knut Bjørnar Wålberg <knut.bjornar.walberg@nav.no>
2025-10-15 15:30:34 +0200

    TEN-1341 - adresseendringer i ACL4.4 for familieområdet

--- src/test/kotlin/no/nav/eux/adresse/oppdatering/AdresseOppdateringTest.kt
+++ src/test/kotlin/no/nav/eux/adresse/oppdatering/AdresseOppdateringTest.kt
@@ -38,8 +38,8 @@ class AdresseOppdateringTest : AbstractTest() {
         "/api/v1/endringer" requestNumber 10 shouldEqual "/dataset/forventet/endringer-bostedsadresse.json"
         kafkaTopicRinaDocumentEvents send case_8F001
         await until { requestBodies["/api/v1/endringer"]?.size == 13 }
-        "/api/v1/endringer" requestNumber 8 shouldEqual "/dataset/forventet/endringer-f001-ektefelle.json"
-        "/api/v1/endringer" requestNumber 9 shouldEqual "/dataset/forventet/endringer-f001-annenperson.json"
+        "/api/v1/endringer" requestNumber 11 shouldEqual "/dataset/forventet/endringer-f001-ektefelle.json"
+        "/api/v1/endringer" requestNumber 12 shouldEqual "/dataset/forventet/endringer-f001-annenperson.json"
     }
 
     @AfterEach
